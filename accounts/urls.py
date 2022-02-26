f7c5a8a102730a919fbe63a475152051a95f6267
Vishal Jaiswal <72380024+Jvishal454@users.noreply.github.com>
2022-02-26 21:44:08 +0530

    Initial commit

---
+++ accounts/urls.py
@@ -0,0 +1,8 @@
+from .views import RegisterAPI, LoginAPI
+from knox import views as knox_views
+from django.urls import path
+
+urlpatterns = [
+    path('api/register/', RegisterAPI.as_view(), name='register'),
+    path('api/login/', LoginAPI.as_view(), name='login'),
+]
